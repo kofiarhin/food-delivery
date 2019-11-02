77c8eb3db3425c44c32767fd3dddcc33cdb30266
Kofi <info@kofiarhin.com>
2019-11-02 11:35:22 +0000

    update

---
+++ build/precache-manifest.a5189b915b491ed470670c3cb88e5583.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bc8f7274abb46078e6c859b1c0ff16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9af81c6da097096eede1",
+    "url": "/static/css/main.cb338ad2.chunk.css"
+  },
+  {
+    "revision": "99ef444693a691116a40",
+    "url": "/static/js/2.eacf5778.chunk.js"
+  },
+  {
+    "revision": "9af81c6da097096eede1",
+    "url": "/static/js/main.61e52e20.chunk.js"
+  },
+  {
+    "revision": "89d94d638289bf35c199",
+    "url": "/static/js/runtime~main.23b9edd6.js"
+  }
+]);
